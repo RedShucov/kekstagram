8d84a5c25734566392f3357bd77553f5b35b4072
RedShucov <redshucov@gmail.com>
2023-06-04 12:47:05 +0300

    Отредактирует обработчики событий модуля upload-form.js, подключит модуль к main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { createPhotosData } from './mock/mock-data-generator.js';
 import { renderThumbnails } from './render-data/render-thumbnails.js';
+import { addModalHandlers } from './picture-uploader/ulpoad-form.js';
 
 const photosData = createPhotosData();
 
 renderThumbnails(photosData);
+
+addModalHandlers();
--- js/picture-uploader/ulpoad-form.js
+++ js/picture-uploader/ulpoad-form.js
@@ -1,27 +1,50 @@
-import { isEscapeKey } from '../util.js';
+import { isEscapeKey, isItFocus } from '../util.js';
 
 const imageUploadForm = document.querySelector('#upload-select-image');
 const imageUploadInput = imageUploadForm.querySelector('#upload-file');
 const imageUploadInterface = imageUploadForm.querySelector('.img-upload__overlay');
+const imageUploadClosure = imageUploadForm.querySelector('#upload-cancel');
 
 const openUploadInterface = () => {
   document.body.classList.add('modal-open');
   imageUploadInterface.classList.remove('hidden');
 
-  document.addEventListener('keydown', onUploadKeydown);
+  document.addEventListener('keydown', keydownOnnUploadFormHandler);
 };
 
 const closeUploadInterface = () => {
   document.body.classList.remove('modal-open');
   imageUploadInterface.classList.add('hidden');
 
-  document.removeEventListener('keydown', onUploadKeydown);
+  document.removeEventListener('keydown', keydownOnnUploadFormHandler);
 };
 
-function onUploadKeydown(event) {
-  if (isEscapeKey(event)) {
+function keydownOnnUploadFormHandler(event) {
+  if (isEscapeKey(event) && !isItFocus(event, 'hashtags', 'description')) {
     closeUploadInterface();
   }
 }
 
-imageUploadInput.addEventListener('change', openUploadInterface);
+/**
+ * Функция обработчик открытия модального окна.
+ */
+const onOpenModalHandler = () => {
+  openUploadInterface();
+};
+
+/**
+ * Функция обработчик закрытия модального окна.
+ */
+const onCloseModalHandler = () => {
+  closeUploadInterface();
+};
+
+/**
+ * Инициализация обработчиков для модального окна.
+ */
+const addModalHandlers = () => {
+  imageUploadInput.addEventListener('change', onOpenModalHandler);
+  imageUploadClosure.addEventListener('click', onCloseModalHandler);
+};
+
+export { addModalHandlers };
