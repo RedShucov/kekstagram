07a246bbd8a08c538dfd8b4d969bfbe42d8fe0a0
RedShucov <redshucov@gmail.com>
2023-06-12 22:14:37 +0300

    Добавит в модуль upload-form.js отдельные функции для добавления и удаления обработчиков событий для работы с формой

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 import { createPhotosData } from './mock/mock-data-generator.js';
 import { renderThumbnails } from './render-data/render-thumbnails.js';
-import { addModalHandlers } from './picture-uploader/ulpoad-form.js';
+import { addOpenModalHandlers } from './picture-uploader/ulpoad-form.js';
 import { addUserFormSubmitHandler } from './picture-uploader/validate-form.js';
 
 const photosData = createPhotosData();
 
 renderThumbnails(photosData);
 
-addModalHandlers();
+addOpenModalHandlers();
 addUserFormSubmitHandler();
--- js/picture-uploader/ulpoad-form.js
+++ js/picture-uploader/ulpoad-form.js
@@ -8,64 +8,70 @@ const imageUploadInterface = imageUploadForm.querySelector('.img-upload__overlay
 const imageUploadClosure = imageUploadForm.querySelector('#upload-cancel');
 
 /**
- * Функция для открытия интерфейса загрузки и редактирования изображения.
+ * Функция, для открытия интерфейса загрузки и редактирования изображения.
  */
 const openUploadInterface = () => {
   showModal(imageUploadInterface);
-
+  addCloseModalHandlers();
   addPreviewScaleHandlers();
   initializeEffectsSettings();
-
-  // Объединить в функцию
-  imageUploadClosure.addEventListener('click', closeModalHandler);
-  document.removeEventListener('keydown', keydownUploadFormHandler);
-  // Объединить в функцию
 };
 
 /**
- * Функция для закрытия интерфейса загрузки и редактирования изображения.
+ * Функция, для закрытия интерфейса загрузки и редактирования изображения.
  */
 const closeUploadInterface = () => {
   hideModal(imageUploadInterface);
-
+  removeCloseModalHandlers();
   removePreviewScaleHandlers();
   deinitializeEffectsSettings();
-
-  // Объединить в функцию
-  imageUploadClosure.removeEventListener('click', closeModalHandler);
-  document.removeEventListener('keydown', keydownUploadFormHandler);
-  // Объединить в функцию
 };
 
 /**
- * Функция обработчик открытия модального окна.
+ * Функция, обработчик закрытия модального окна.
+ */
+const closeModalHandler = () => {
+  closeUploadInterface();
+};
+
+/**
+ * Функция, обработчик для закрытия интрефейса добавления новой фотографии при нажатие на клавишу-ESC.
+ * @param {KeyboardEvent} evt - Объект события нажатия клавиши клавиатуры.
+ */
+const keydownUploadFormHandler = (evt) => {
+  if (isEscapeKey(evt) && !isItFocus(evt, 'hashtags', 'description')) {
+    closeUploadInterface();
+  }
+};
+
+/**
+ * Инициализация обработчиков для закрытия интерфейса добавления новой фотографии.
+ */
+function addCloseModalHandlers() {
+  imageUploadClosure.addEventListener('click', closeModalHandler);
+  document.addEventListener('keydown', keydownUploadFormHandler);
+}
+
+/**
+ * Удаление обработчиков для закрытия интерфейса добавления новой фотографии.
+ */
+function removeCloseModalHandlers() {
+  imageUploadClosure.removeEventListener('click', closeModalHandler);
+  document.removeEventListener('keydown', keydownUploadFormHandler);
+}
+
+/**
+ * Функция, обработчик открытия модального окна.
  */
 const openModalHandler = () => {
   openUploadInterface();
 };
 
 /**
- * Функция обработчик закрытия модального окна.
- */
-function closeModalHandler() {
-  closeUploadInterface();
-}
-
-/**
- * Функция, обработчик для закрытия интрефейса добавления новой фотографии при нажатие на клавишу-ESC.
- * @param {KeyboardEvent} evt - Объект события нажатия клавиши клавиатуры.
- */
-function keydownUploadFormHandler(evt) {
-  if (isEscapeKey(evt) && !isItFocus(evt, 'hashtags', 'description')) {
-    closeUploadInterface();
-  }
-}
-
-/**
  * Инициализация обработчиков для модального окна.
  */
-const addModalHandlers = () => {
+const addOpenModalHandlers = () => {
   imageUploadInput.addEventListener('change', openModalHandler);
 };
 
-export { addModalHandlers };
+export { addOpenModalHandlers };
