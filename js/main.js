a540edd1a6bfe51f606e5c4702743d34bf5432bb
RedShucov <redshucov@gmail.com>
2023-07-12 12:36:19 +0300

    Заменит использование данных с моковых на ответ с сервера для отрисовки миниатюр в модуле main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-import { createPhotosData } from './mock/mock-data-generator.js';
 import { renderThumbnails } from './render-data/render-thumbnails.js';
 import { addOpenModalHandlers } from './picture-uploader/ulpoad-form.js';
 import { addUserFormSubmitHandler } from './picture-uploader/validate-form.js';
+import { getData } from './api/server.js';
 
-const photosData = createPhotosData();
-
-renderThumbnails(photosData);
+getData(renderThumbnails);
 
 addOpenModalHandlers();
 addUserFormSubmitHandler();
